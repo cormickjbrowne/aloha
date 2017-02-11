e9804b8fcdac284220df9b10711d732fb4a9c435
NYCSwan <megan.swanby@gmail.com>
2017-02-11 11:53:10 -0500

    add port to index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express');
 
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.set('port', (process.env.PORT || 3000));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port 3000!', app.get('port'));
+});
